0b04d499c016cbf11d9cf5bb770cf1a4e478f0e7
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-26 02:15:06 -0300

    Add files via upload

---
+++ graph-algorithms/kruskal.cpp
@@ -0,0 +1,27 @@
+void kruskal() {
+    Graph result(numNode);
+    priority_queue<Edge> heap;
+
+    for (int i = 0; i < numNode; ++i) {
+        for (auto j:adj[i]) {
+            heap.push({i, j.first, j.second});
+        }
+    }
+
+    unionFind uf(numNode);
+    int aux = numNode;
+
+    while (aux > 1) {
+        auto temp = heap.top();
+        heap.pop();
+
+        int rt1 = uf.find(temp.nodeAtual);
+        int rt2 = uf.find(temp.nodeNext);
+
+        if (rt1 != rt2) {
+            uf.merge(rt1, rt2);
+            result.setEdge(rt1, rt2, temp.weight);
+            aux--;
+        }
+    }
+};
