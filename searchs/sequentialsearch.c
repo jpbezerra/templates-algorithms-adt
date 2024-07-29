4237b62e08e93807f3b388b5b58761160e90b05b
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-29 20:14:54 -0300

    Update sequentialsearch.c

--- searchs/sequentialsearch.c
+++ searchs/sequentialsearch.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void search(int arr[], int size, int number) {
+void sequentialSearch(int arr[], int size, int number) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == number) {
             printf("The number %d exists in this array!", number);
             return;
         }
     }
-    
+
     printf("The number %d doesn't exists in this array!", number);
 }
 
@@ -15,7 +15,7 @@ int main() {
     int arr[] = {64, 35, 78, 12, 91, 84, 85, 32, 67}; // don't need to be sorted
     int size = sizeof(arr)/sizeof(arr[0]);
     int number = 123;
-    search(arr, size, number); 
+    sequentialSearch(arr, size, number);
 
     return 0;
 }
