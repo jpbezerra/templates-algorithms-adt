340ccb99a5370f70599c63c600e23fd110e7690a
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-30 12:12:12 -0300

    Update selectionsort.c

--- sorts/selectionsort.c
+++ sorts/selectionsort.c
@@ -19,20 +19,3 @@ void selectionSort(int arr[], int size) {
         swap(&arr[i], &arr[min]);
     }
 }
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\nFinish!");
-}
-
-int main() {
-    int arr[] = {64, 35, 78, 12, 91, 84, 85, 32, 67};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, size);
-    printArray(arr, size);
-
-    return 0;
-}
