cbf73b002b45ef5d25486f6e1ff6dd0add231627
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-30 12:11:40 -0300

    Update insertionsort.c

--- sorts/insertionsort.c
+++ sorts/insertionsort.c
@@ -13,20 +13,3 @@ void insertionSort(int arr[], int size) {
         arr[j + 1] = v;
     }
 }
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\nFinish!");
-}
-
-int main() {
-    int arr[] = {64, 35, 78, 12, 91, 84, 85, 32, 67};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    insertionSort(arr, size);
-    printArray(arr, size);
-
-    return 0;
-}
